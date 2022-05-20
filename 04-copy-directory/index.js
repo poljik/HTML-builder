b15349d7977fb5f7feeecbc1bbe6a0a5e4b4198f
poljik <poljik@gmail.com>
2022-05-20 18:48:11 +0300

    fix: remove console.log for 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -2,7 +2,6 @@ const fs = require('fs/promises');
 const path =  require('path');
 const pathFrom = path.join(__dirname,'files');
 const pathTo = path.join(__dirname,'files-copy');
-console.log(pathTo);
 
 (async function () {
   await fs.rm(pathTo, {recursive: true, force: true}, (err) =>{
