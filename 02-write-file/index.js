d8e0cfcc2096345d694a962fec9adccd1975e80e
poljik <poljik@gmail.com>
2022-05-20 12:47:17 +0300

    feat: add code for 02-write-file

---
+++ 02-write-file/index.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const path =  require('path');
+
+const pathFile = path.join(__dirname,'text.txt');
+const output = fs.createWriteStream(pathFile);
+
+console.log("Please, enter something funny here:");
+
+process.stdin.on('data', txt => {
+	if(txt.toString().slice(0,-1) === "exit") process.exit();
+	output.write(txt);
+});
+
+process.on('exit', () => console.log("Bye-bye! :)"));
+process.on('SIGINT', () => process.exit());
